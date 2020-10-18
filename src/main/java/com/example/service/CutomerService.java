56b9399c992ab0361ae19673c462519a530cd0d4
Yusuke Watanabe <you-sk@pc.email.ne.jp>
2020-10-18 17:27:33 +0900

    1st commit

---
+++ src/main/java/com/example/service/CutomerService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CutomerService {
+//	@Autowired
+//	CusomerRepository 
+
+}
